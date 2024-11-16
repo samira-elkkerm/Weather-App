660a42d2565ae1f08367a26e1f4d46937b706fc2
Samira elkkerm <samiraelkkerm14@gmail.com>
2024-11-16 20:43:19 +0100

    1er commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFrown } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function Grp204WeatherApp() {
   const [input, setInput] = useState("");
